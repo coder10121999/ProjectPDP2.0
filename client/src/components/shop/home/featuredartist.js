f8a801cacec15e34a65c9c1ab83e7dac02c4c942
Geethika Reddy <56604965+Geethika44@users.noreply.github.com>
2022-04-03 16:16:41 +0530

    featuredArtist style changes

--- client/src/components/shop/home/featuredartist.js
+++ client/src/components/shop/home/featuredartist.js
@@ -11,7 +11,17 @@ const featuredArtist =()=>{
         <Fragment>
             <div className="flex-grow">
                 <Navber />
-                <FeaturedArtistinfo/>
+                <div style={{paddingTop:"8rem",paddingBottom:"1rem",textAlign:"center"}}>
+                    <span
+                    style={{ letterSpacing: "0.10rem",textAlign:"center"}}
+                    className="text-center font-bold uppercase text-gray-800 text-2xl text-center"
+                    >Featured Artists</span>
+                </div>
+                <hr />
+                <div  className="m-4 md:mx-8 md:my-6 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4" 
+                style={{display:"flex",justifyContent:"space-between",flexDirection:"row",flexWrap:"wrap",paddingTop:"2rem",paddingLeft:"2rem",paddingRight:"2rem"}}>
+                    <FeaturedArtistinfo/>
+                </div>
             </div>
             
             <Footer/>
